b24bc34690a135a70e4aa1407e7d1264a3da547c
salzteam <akshalrizkigandari@gmail.com>
2022-11-14 21:27:53 +0700

    transaction

--- src/middlewares/isLogin.js
+++ src/middlewares/isLogin.js
@@ -16,6 +16,7 @@ module.exports = () => {
         next();
       })
       .catch((err) => {
+        console.log(err);
         return res
           .status(401)
           .json({ msg: "You have to login first", data: null });
