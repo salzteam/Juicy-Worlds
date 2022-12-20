b6cebfd37ca59d19ab402ddd57b837c3d744fd72
salzteam <akshalrizkigandari@gmail.com>
2022-12-20 20:35:18 +0700

    Add Remote Notification

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,4 +1,11 @@
 const express = require("express");
+const { initializeApp, applicationDefault } = require("firebase-admin/app");
+const { Messaging } = require("firebase-admin/messaging");
+
+const app = initializeApp({
+  credential: applicationDefault(),
+});
+const Notification = new Messaging(app);
 
 const usersRouter = require("./users");
 const productsRouter = require("./products");
@@ -17,6 +24,24 @@ mainRouter.get("/", (req, res) => {
   res.json({
     msg: "Welcome",
   });
+});
+mainRouter.post(`/notification`, async (req, res) => {
+  const { body } = req;
+  const token =
+    "f7m9vCe5SdaCrmdYchSHR6:APA91bEqVr5V1LLN3Cyzd6ID87cYxu0pNGgOWxo6BFFRCXvBUmvoDFSG8rODybasACFv7u9Dc7wTjottzNka_q120Ze4jstZFWQ_gVk42U3uBs6Dd8tnZNzkrYCmmh0c28KEGq5RUv2l";
+  try {
+    await Notification.send({
+      token,
+      notification: {
+        body: body.message,
+        title: body.title,
+      },
+    });
+    res.status(200).json({ message: "Notification sent successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "System Error" });
+  }
 });
 
 module.exports = mainRouter;
